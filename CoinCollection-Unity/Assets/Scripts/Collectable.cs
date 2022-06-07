f7e4c7b44063bf93dc1cecc8f4670f1a64597e8e
Professor Akram <professorakramburris@gmail.com>
2022-06-06 22:52:31 -0500

    Collectable Script
    start of the collectable script

---
+++ CoinCollection-Unity/Assets/Scripts/Collectable.cs
@@ -0,0 +1,28 @@
+/**** 
+ * Created by: Stu Dent
+ * Date Created: Feb 3, 2022
+ * 
+ * Last Edited by: 
+ * Last Edited: 
+ * 
+ * Description: Collectable object behaviors for counting the total amount of collectables and checking for collision with the player.
+****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Collectable created!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
