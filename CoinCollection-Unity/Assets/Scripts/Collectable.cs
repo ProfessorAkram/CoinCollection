986cff0cf46f84d95ba1e3c85a5e245783e8a523
Professor Akram <professorakramburris@gmail.com>
2022-06-07 01:25:05 -0500

    Collectable on trigger added

--- CoinCollection-Unity/Assets/Scripts/Collectable.cs
+++ CoinCollection-Unity/Assets/Scripts/Collectable.cs
@@ -14,15 +14,33 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    /***VARIABLES***/
+    static public int collectableCount; //counts the number of colletables in the scene
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Collectable created!"); 
-    }
+        collectableCount++; //add to collectable
+        Debug.Log("Number of Colletables " + collectableCount);
+    }//end Start()
 
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }//end Update()
+
+    //Called when a GameObject collides with another GameObject
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collectable Triggered");
+
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject); //destroy this gameObject (collectable object)
+        }
+
+    }//end OnTriggerEnter()
+
+
 }
